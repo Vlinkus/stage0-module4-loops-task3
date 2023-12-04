162e757554bb534f4983b0f46055c83a91974ec5
VLinkus <LinkusVyte@gmail.com>
2023-12-04 10:37:54 +0200

    M4  Task Loops3 Completed

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int first = 0, second = 1, fib;
+        System.out.println(first+"\n"+second);
+        for (int i = 2; i < lastFibonacci; i++) {
+            fib = first + second;
+            System.out.println(fib);
+            first =  second;
+            second = fib;
+
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        int result = 0;
+        int numberLength = 1;
+        for (int i = 0; i <= lengthOfLastNumber; i++) {
+            if(i <= 0){
+                result = 0;
+            }else {
+                result += numberLength*9;
+                numberLength *= 10;
+                if(numberLength % 10 != 1){
+                    numberLength++;
+                }
+            }
+        }
+        System.out.println(result);
     }
 }
